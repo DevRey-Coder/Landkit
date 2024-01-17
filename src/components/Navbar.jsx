f95ff9b42f242f3ee54017c1938d1dcec6af2d35
Lextor-lb <seriousglobin@gmail.com>
2024-01-17 14:54:39 +0630

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {navLinks} from "../constants/index.js"
+import Logo from '../assets/Logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='w-full'>
+      <div className='flex '>
+      <div>
+<img src={Logo} alt="" /></div>
+</div>
     </div>
   )
 }
